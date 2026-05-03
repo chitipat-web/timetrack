f1271ff9b6939388ffb12ae714450072386692a7
chitipat-web <chitipat.kao@gmail.com>
2026-05-03 12:08:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ===== TimeTrack Service Worker =====
-const CACHE_NAME = 'timetrack-v30';
-const STATIC_CACHE = 'timetrack-static-v30';
-const FIREBASE_CACHE = 'timetrack-firebase-v30';
+const CACHE_NAME = 'timetrack-v31';
+const STATIC_CACHE = 'timetrack-static-v31';
+const FIREBASE_CACHE = 'timetrack-firebase-v31';
 
 const STATIC_ASSETS = [
   './index.html',
