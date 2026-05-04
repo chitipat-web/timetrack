cbeba392633fcb3b86f44559028adc09f68b4f92
chitipat-web <chitipat.kao@gmail.com>
2026-05-04 08:50:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== RUDY Service Worker =====
-const CACHE_NAME = 'rudy-v40';
+const CACHE_NAME = 'rudy-v41';
 const STATIC_CACHE = 'rudy-static-v32';
 const FIREBASE_CACHE = 'rudy-firebase-v32';
 
